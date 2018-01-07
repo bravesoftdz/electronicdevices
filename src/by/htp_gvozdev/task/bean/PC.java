752edea43afcb755089bfac8f322d33ae0a19254
Alexsander Gvozdev <a.gvozdev@outlook.com>
2018-01-07 17:26:31 +0300

    Add files via upload

---
+++ src/by/htp_gvozdev/task/bean/PC.java
@@ -0,0 +1,9 @@
+package by.htp_gvozdev.task.bean;
+
+public final class PC extends Production {
+
+	public PC(String name, String type, double power, double consumption, boolean turn) {
+		super(name, type, power, consumption, turn);
+	}
+
+}
--- src/by/htp_gvozdev/task/logic/Initialization.java
+++ src/by/htp_gvozdev/task/logic/Initialization.java
@@ -4,7 +4,9 @@ import java.util.ArrayList;
 
 import by.htp_gvozdev.task.bean.Kettle;
 import by.htp_gvozdev.task.bean.Lamp;
+import by.htp_gvozdev.task.bean.PC;
 import by.htp_gvozdev.task.bean.Production;
+import by.htp_gvozdev.task.bean.TV;
 import by.htp_gvozdev.task.bean.Сooker;
 
 public class Initialization {
@@ -18,9 +20,13 @@ public class Initialization {
 
 	public static ArrayList<Production> setAppliances() {
 
-		list.add(new Kettle("LG", "Kettle", 20.00, 2.00, true));
-		list.add(new Сooker("Samsung", "Cooker", 50.00, 4.00, true));
+		list.add(new Kettle("LG", "Kettle", 20.00, 2.00, false));
+		list.add(new Сooker("Samsung", "Cooker", 50.00, 4.00, false));
 		list.add(new Lamp("Phileps", "Lamp", 10.57, 7.50, true));
+		list.add(new TV("Sony", "TV", 5, 14, false));
+		list.add(new PC("Lenovo", "PC", 5, 14, true));
+		
+		
 
 		return list;
 	}
